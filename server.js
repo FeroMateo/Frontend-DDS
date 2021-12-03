60d3933e11d713b13c99f3c949ce38e2ad207edb
feromateo <feromateo2000>
2021-12-03 01:51:45 -0300

    Ahora

--- server.js
+++ server.js
@@ -5,6 +5,3 @@ app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
-app.use('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'https://tp-dds-g7.herokuapp.com/'));
-});
