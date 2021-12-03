48d439dba6cf0047671b080d2d8887210357b358
feromateo <feromateo2000>
2021-12-03 01:32:21 -0300

    deploy

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
 app.use('*', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'https://tp-dds-g7.herokuapp.com/'));
 });
-const port = process.env.PORT || 80;
-app.listen(port);
